4731a79885b4c79d380ee3f508c9918469f2081f
AmrElbaz <amrelbaz123456@gmail.com>
2020-02-07 17:15:48 +0200

    modifying module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module org.project {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
+    requires java.rmi;
 
     opens org.project to javafx.fxml;
     exports org.project;
