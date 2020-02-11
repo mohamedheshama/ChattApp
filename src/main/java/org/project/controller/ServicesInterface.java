c5708c2cc40d801b92cb737a7613e4de8e25c739
AmrElbaz <amrelbaz123456@gmail.com>
2020-02-11 17:13:46 +0200

    modifying ServicesImp and ServicesInterface

--- src/main/java/org/project/controller/ServicesInterface.java
+++ src/main/java/org/project/controller/ServicesInterface.java
@@ -2,14 +2,17 @@ package org.project.controller;
 
 import org.project.exceptions.UserAlreadyExistException;
 import org.project.model.dao.users.Users;
+
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 import java.util.ArrayList;
 
 public interface ServicesInterface extends Remote{
     public Users getUserData(String phoneNumber) throws RemoteException;
-    public Boolean Register(Users user) throws RemoteException, UserAlreadyExistException;
-    public Boolean checkUserLogin(String phoneNumber,String password) throws RemoteException;
+
+    public Boolean register(Users user) throws RemoteException, UserAlreadyExistException;
+
+    public Boolean checkUserLogin(String phoneNumber, String password) throws RemoteException;
     public ArrayList<Users> getFriends(String phoneNumber)throws RemoteException;
     public ArrayList<Users> getNotifications(String phoneNumber) throws RemoteException;
     public void notifyUpdate(Users users) throws RemoteException;
